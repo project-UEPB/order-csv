9210e8d9a28c4d537acb7537b4ff3b5a962b0670
Klayton Junior <55154107+KlaytonJr@users.noreply.github.com>
2021-09-03 12:07:14 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import Console.Console;
 public class Main {
   public static void main(String[] args) {
     
-    Console console = new Console("caso_full_tratado.csv");
+    Console console = new Console("nomeDoArquivo.csv");
     console.menu();
 
   }
